4a84d56173e5f29967446a91b0bd785ec8d67889
Ygor <ygor.maniz@gmail.com>
2020-06-10 00:08:48 -0300

    Composing with Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+class ContactList extends Component {
+    render() {
+        const people = this.props.contacts
+
+        return <ol>
+            {people.map((person) => (
+                <li key={person.name}>{person.name}</li>
+            ))}
+        </ol>
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ContactList contacts={[
+                {name: 'Ygor'},
+                {name: 'Hamilton'},
+                {name: 'Flavia'}
+            ]}/>
+            <ContactList contacts={[
+                {name: 'Amanda'},
+                {name: 'Mario'},
+                {name: 'Helena'}
+            ]}/>
+        </div>
+    );
 }
 
 export default App;
